0e131cd2fbc48aeb9ac546760dc922a1b4b56fee
MohammadHossein <5309489-mohammadhosseinbagheri@users.noreply.gitlab.com>
2021-02-11 21:09:36 +0330

    login api

--- src/models/Teacher.js
+++ src/models/Teacher.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
-const ManagerSchema = mongoose.Schema({
+const TeacherSchema = mongoose.Schema({
   name: { type: String, required: true },
   lastName: { type: String, required: true },
   nationalCode: { type: String, required: true, unique: true },
   //   birthday: { type: Date, required: true },
   birthday: { type: String, required: true },
   phone: { type: String, required: true },
-  email: { type: String, required: true },
+  courses: { type: Object, default: {} },
   address: { type: String, required: true },
   password: { type: String, required: true },
 });
-const ManagerModel = mongoose.model("manager", ManagerSchema);
-module.exports = ManagerModel;
+const TeacherModel = mongoose.model("teacher", TeacherSchema);
+module.exports = TeacherModel;
--- src/routes/auth/login/login.js
+++ src/routes/auth/login/login.js
@@ -1,24 +1,87 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
 const StudentModel = require("../../../models/Student");
-router.post("/", (req, res) => {
-  const { password, nationalCode } = req.body;
-  //manager
-  StudentModel.find({ nationalCode: nationalCode }, async (error, response) => {
-    if (error) {
-      res.json(error);
-    } else if (response.length !== 0) {
-      const dbPassword = await response[0].password;
-      if (dbPassword !== password) {
-        res.json("اطلاعات ورودی اشتباه است");
-      } else {
-        res.json(...response);
-      }
-    } else {
-      res.json("چنین کاربری وجود ندارد");
-      //teacher
+const ManagerModel = require("../../../models/Manager");
+const TeacherModel = require("../../../models/Teacher");
+
+router.post(
+  "/",
+  // check("natinalCode").isLength({ min: 10, max: 10 }),
+  // check("password").isLength({ min: 8 }),
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
     }
-  });
-});
+    const { password, nationalCode } = req.body;
+    //manager
+    ManagerModel.find(
+      { nationalCode: nationalCode },
+      async (error, response) => {
+        // console.log(...response);
+        if (error) {
+          res.json(error);
+        } else if (response.length !== 0) {
+          const dbPassword = await response[0].password;
+          if (dbPassword !== password) {
+            res.json("اطلاعات ورودی اشتباه است");
+          } else {
+            const target = { data: response[0] };
+            const source = { type: "manager" };
+
+            const returnedTarget = Object.assign(target, source);
+            res.json(returnedTarget);
+          }
+        }
+        return;
+      }
+    );
+    StudentModel.find(
+      { nationalCode: nationalCode },
+      async (error, response) => {
+        console.log(...response);
+        if (error) {
+          res.json(error);
+        } else if (response.length !== 0) {
+          const dbPassword = await response[0].password;
+          if (dbPassword !== password) {
+            res.json("اطلاعات ورودی اشتباه است");
+          } else {
+            const target = { data: response[0] };
+            const source = { type: "student" };
+
+            const returnedTarget = Object.assign(target, source);
+            res.json(returnedTarget);
+          }
+        } else {
+          TeacherModel.find(
+            { nationalCode: nationalCode },
+            async (err, ress) => {
+              console.log(...ress);
+              if (err) {
+                res.json(err);
+              } else if (ress.length !== 0) {
+                const dbPass = await ress[0].password;
+                if (dbPass !== password) {
+                  res.json("اطلاعات ورودی اشتباه است");
+                } else {
+                  const target = { data: ress[0] };
+                  const source = { type: "teacher" };
+
+                  const returnedTarget = Object.assign(target, source);
+                  res.json(returnedTarget);
+                }
+              } else {
+                res.json("چنین کاربری وجود ندارد");
+              }
+              return;
+            }
+          );
+        }
+      }
+    );
+  }
+);
 
 module.exports = router;
