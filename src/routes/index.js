75eff86d879fe0c0d984651d3a3b2ef73d40642a
MohammadHossein <5309489-mohammadhosseinbagheri@users.noreply.gitlab.com>
2021-02-12 21:39:33 +0330

    add register api

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const loginApi = require("./auth/login/login");
+const registerApi = require("./auth/register/register");
 router.use("/login", loginApi);
+router.use("/register", registerApi);
 
 module.exports = router;
